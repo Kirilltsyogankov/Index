f59abdac14eb679921e2b25f8470761a0a5e3cf4
opilane <opilane@HP-CZC2349HV2>
2024-01-18 12:22:57 +0200

    Add project files.

---
+++ Index/Index.py
@@ -0,0 +1,10 @@
+﻿print("Tere! Olen sinu uus sõber - Python!")
+nimi=input("Susenda oma nimi.")
+print(nimi, "oi kui ilus nimi!")
+number=int(input(nimi, "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => "))
+if=vastus ==1:
+    pikkus=int(input("Palun sisenda oma pikkus.", int))
+    mass=float(input("Palun sisenda oma kaal", float))
+    index=(mass/0.01=pikkus)**2,1)
+    print(nimi+*)
+
